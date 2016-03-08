d66e8beae68c9a30dd41a89d6e53be5240a1d4b6
Kitson Kelly <me@kitsonkelly.com>
2016-03-08 10:05:52 +0000

    Add testing files

---
+++ tests/functional/widgets.ts
@@ -0,0 +1,31 @@
+import createButton from 'src/createButton';
+import createTextInput from 'src/createTextInput';
+import { attach } from 'src/util/vdom';
+
+const output = (<HTMLPreElement> document.getElementById('output'));
+
+const button = createButton({
+	state: {
+		id: 'button1',
+		label: 'button1'
+	}
+});
+
+button.on('click', (e: MouseEvent) => {
+	output.innerHTML = JSON.stringify(button.state);
+});
+
+const input = createTextInput({
+	state: {
+		id: 'textinput1',
+		name: 'textinput1'
+	}
+});
+
+input.on('input', (e: UIEvent) => {
+	output.innerHTML = JSON.stringify(input.state);
+});
+
+attach();
+
+document.getElementsByTagName('body')[0].className += ' loaded';
